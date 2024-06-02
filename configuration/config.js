a0710bb5585a57a95414bce8fcde4e77beb4030b
samarthwtf <samarthplayz1337@gmail.com>
2024-06-02 22:42:37 +0530

    updated configurations

--- configuration/config.js
+++ configuration/config.js
@@ -7,12 +7,12 @@ const fs = require('fs');
 const doc = yaml.load(fs.readFileSync('./config.yml', 'utf8'));
 
 module.exports = {
-  token: process.env.TOKEN ?? doc.bot?.TOKEN ?? 'MTE4NDcwNDYyNjk1MjUxMTUzOA.GKlbTm.dy-4WHGULf-OS03FKwPFlGw9RJfXGya7JEzjzc',
+  token: process.env.TOKEN ?? doc.bot?.TOKEN ?? '',
   owners: process.env.OWNERS ?? doc.bot?.OWNERS ?? ['OWNER_1', 'OWNER_2'],
   prefix: process.env.PREFIX ?? doc.bot?.PREFIX ?? 'YOUR_PREFIX',
   clientID: process.env.CLIENT_ID ?? doc.bot?.ID ?? 'YOUR_CLIENT_ID',
   embedColor: process.env.COLOR ?? doc.bot?.EMBED_COLOR ?? 'HEX_COLOR',
-  mongo: process.env.MONGO ?? doc.bot?.MONGO ?? 'mongodb+srv://neonplayz:Neon@1337@cluster0.hz4ryoh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  mongo: process.env.MONGO ?? doc.bot?.MONGO ?? '',
 
   links: {
     invite: process.env.INVITE ?? doc.links?.INVITE ?? 'YOUR_WEBSITE',
